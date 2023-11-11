970b1092408ac44d8e76a24bf824b16031efc6ca
gitdoyeon <gitpdyeon@gmail.com>
2023-11-11 13:09:43 +0900

    alert창

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,24 @@
-function addMapClicked() {
-    alert("지도에 추가 되었습니다");
-    // Additional logic for handling the "Add to Map" click event
-}
+var addedLocations = [];
 
-function addBookmarkClicked() {
-    alert("북마크에 추가 되었습니다");
-    // Additional logic for handling the "Add to Bookmark" click event
-}
+    function addMapClicked(location) {
+        if (!isLocationAdded(location)) {
+            alert("지도에 추가 되었습니다");
+            addedLocations.push(location);
+        } else {
+            alert("이미 추가된 위치입니다");
+        }
+    }
+
+    function addBookmarkClicked(location) {
+        if (!isLocationAdded(location)) {
+            alert("북마크에 추가 되었습니다");
+            addedLocations.push(location);
+        } else {
+            alert("이미 추가된 위치입니다");
+        }
+    }
+
+    function isLocationAdded(location) {
+        // 위치가 이미 배열에 있는지 확인
+        return addedLocations.includes(location);
+    }
