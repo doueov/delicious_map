fa187203738755316a669a4610794cf0daaa3930
gitdoyeon <gitpdyeon@gmail.com>
2023-11-11 13:01:46 +0900

    alert()추가

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+function addMapClicked() {
+    alert("지도에 추가 되었습니다");
+    // Additional logic for handling the "Add to Map" click event
+}
+
+function addBookmarkClicked() {
+    alert("북마크에 추가 되었습니다");
+    // Additional logic for handling the "Add to Bookmark" click event
+}
